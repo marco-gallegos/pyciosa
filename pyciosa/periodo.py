efbb59f8fe62a94c5e2a7860a9ade7a05d1dac2e
marco gallegos <ma_galeza@hotmail.com>
2020-04-11 03:39:12 -0500

    [imp] periodo: interpolar, get_periodo
    se implementa un funcion en el modulo para interpolar periodos partiendo
    de un periodo, tambien se implementa una funcion para regresar un
    periodo partiendo de una fecha en datetime nativo o partiendo de una
    fecha pendulum

--- pyciosa/periodo.py
+++ pyciosa/periodo.py
@@ -34,3 +34,40 @@ def explode(periodo:str)-> (int, int):
             return None, None
     else:
             return None, None
+
+
+def get_periodo(date:datetime.date|pendulum.DateTime)->str:
+    """Regresar el periodo en formato YYYYMM -> 202001 correspondiente a el parametro date"""
+    year = None
+    month = None
+    periodo = None
+    if isinstance(date, datetime.date):
+        year = date.year
+        month = date.month
+        month = f"0{month}" if month < 10 else month 
+        periodo = f"{year}{month}"
+    elif isinstance(date, pendulum.DateTime):
+        periodo = date.format("YMM")
+    if is_valid(periodo):
+        return periodo
+    else:
+        return None
+
+
+def interpolate(periodo:str, rango:int=13)->list:
+    """Regresar un list con los periodos interpolados entre el actual y 'rango'-1 meses anteriores
+    es decir que contando el mes pasado como parametro tengamos 'rango' numero de periodos
+    """
+    year, month = explode(periodo)
+    if year and month:
+        contador = rango
+        fecha = pendulum.datetime(year=year, month=month, day=1)
+        periodos = list()
+        while contador > 0:
+            periodo = get_periodo(fecha)
+            periodos.append(periodo)
+            fecha = fecha.subtract(months=1)
+            contador -= 1
+        return periodos
+    else:
+        return None
